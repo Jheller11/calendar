6b4d617943bf409a67335709c33cb02cbf608b39
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-04-04 15:31:11 -0400

    clean up js/add comments for next steps

--- public/scripts/itemsIndex.js
+++ public/scripts/itemsIndex.js
@@ -1,45 +1,59 @@
-console.log('items index script initialized')
-console.log(items)
+;(function() {
+  'use strict'
 
-const list = document.querySelector('.item-list')
+  console.log('items index script initialized')
 
-class Item {
-  constructor(item) {
-    this.title = item.title
-    this.url = item.url
-    this.created = item.created
-    this.dateTime = item.dateTime
-    this.length = item.length
-    this.recurring = item.recurring
-    this.completed = item.completed
-    this.id = item._id
+  const list = document.querySelector('.item-list')
+
+  class Item {
+    constructor(item) {
+      this.title = item.title
+      this.url = item.url
+      this.created = item.created
+      this.dateTime = item.dateTime
+      this.length = item.length
+      this.recurring = item.recurring
+      this.completed = item.completed
+      this.id = item._id
+    }
+    //   construct DOM element
+    construct() {
+      //  using codepen to create a style/html structure for all info
+      return `<div>${this.title} (${this.created})</div>`
+    }
+    //   mount to DOM
+    mount() {
+      let li = document.createElement('li')
+      li.innerHTML = this.construct()
+      li.setAttribute('id', this.id)
+      list.appendChild(li)
+    }
+
+    //   unmount from DOM
+    unmount() {
+      let target = document.getElementById(this.id)
+      list.removeChild(target)
+    }
+
+    // delete from db
+    // AJAX request to server and return new items then call initialize with new items
+
+    // mark complete
+    // AJAX request to server and return new items then call initialize with new items
   }
 
-  //   mount to DOM
-  mount() {
-    let li = document.createElement('li')
-    li.innerText = this.title
-    li.setAttribute('id', this.id)
-    list.appendChild(li)
+  // initialize list
+  const initialize = items => {
+    let instances = []
+    items.forEach(item => {
+      instances.push(new Item(item))
+    })
+    console.log(instances)
+
+    instances.forEach(item => {
+      item.mount()
+    })
   }
 
-  //   unmount from DOM
-  unmount() {
-    let target = document.getElementById(this.id)
-    list.removeChild(target)
-  }
-
-  // delete from db
-}
-
-let instances = []
-items.forEach(item => {
-  instances.push(new Item(item))
-})
-console.log(instances)
-
-instances.forEach(item => {
-  item.mount()
-})
-
-instances[2].unmount()
+  initialize(items)
+})()
